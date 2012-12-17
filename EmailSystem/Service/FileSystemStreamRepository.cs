848d77ba6ef0e6bf2e08dd3cf08a9fce3d2bed27
JoeBuntu <jcooper@joepserver.net>
2012-12-16 22:30:54 -0500

    Adding Data Stream Repository

---
+++ EmailSystem/Service/FileSystemStreamRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Service
+{
+    public class FileSystemStreamRepository : IDataStreamRepository
+    {
+        public void Add(Stream data, string uniqueIdentifier)
+        {
+            using (FileStream fs = File.OpenWrite(uniqueIdentifier + ".data"))
+            {
+                data.CopyTo(fs);
+            }
+        }
+    }
+}
