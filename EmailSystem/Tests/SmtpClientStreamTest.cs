b45f925860411c4d11e47297fe22432d2cf86c2a
JoeBuntu <jcooper@joepserver.net>
2012-12-19 23:16:57 -0500

    started unit tests

---
+++ EmailSystem/Tests/SmtpClientStreamTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Service;
+
+namespace Tests
+{
+    [TestClass]
+    public class SmtpClientStreamTest
+    { 
+        private StreamReader _Reader;
+        private StreamWriter _Writer;
+        private DuplexCommunicationPipe _twoWayPipe;
+        private SmtpStreamProcessor _StreamProcessor;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _twoWayPipe = new DuplexCommunicationPipe();
+            _Reader = new StreamReader(_twoWayPipe.Stream1);
+            _Writer = new StreamWriter(_twoWayPipe.Stream1);
+            _Writer.NewLine = "\r\n";
+            _Writer.AutoFlush = true;
+
+            _StreamProcessor = new SmtpStreamProcessor(_twoWayPipe.Stream2);
+            new Action(_StreamProcessor.Process).BeginInvoke(null, null);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            _twoWayPipe.Dispose();
+        }
+
+        [TestMethod]
+        public void FirstReplyIs220Greeting()
+        { 
+            string reply = _Reader.ReadLine();
+            Assert.IsNotNull(reply);
+            Assert.IsTrue(reply.StartsWith("220"));
+        }
+
+        [TestMethod]
+        public void AcceptsEHLO()
+        {
+            string reply = _Reader.ReadLine();
+            Assert.IsNotNull(reply);
+            StringAssert.StartsWith(reply, "220");
+     
+            _Writer.WriteLine("EHLO localhost");
+
+            reply = _Reader.ReadLine();
+            Assert.IsNotNull(reply);
+            StringAssert.StartsWith(reply, "250"); 
+        }
+
+        [TestMethod]
+        public void AcceptsHELO()
+        {
+            string reply = _Reader.ReadLine();
+            Assert.IsNotNull(reply);
+            StringAssert.StartsWith(reply, "220");
+
+            _Writer.WriteLine("HELO localhost");
+
+            reply = _Reader.ReadLine();
+            Assert.IsNotNull(reply);
+            StringAssert.StartsWith(reply, "250"); 
+        }
+
+        [TestMethod]
+        public void AcceptsEHLOAndReturnsMultiLine250()
+        {
+            string reply = _Reader.ReadLine();
+            Assert.IsNotNull(reply);
+            StringAssert.StartsWith(reply, "220");
+
+            _Writer.WriteLine("EHLO localhost");
+
+            reply = _Reader.ReadLine();
+            Assert.IsNotNull(reply);
+            StringAssert.StartsWith(reply, "250-SIZE 10000000");
+
+            reply = _Reader.ReadLine();
+            Assert.IsNotNull(reply);
+            StringAssert.StartsWith(reply, "250-STARTTLS");
+
+            reply = _Reader.ReadLine();
+            Assert.IsNotNull(reply);
+            StringAssert.StartsWith(reply, "250 HELP");
+        }
+
+       
+
+    }
+}
---
+++ EmailSystem/Tests/TwoWayCommunicationStream.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Tests
+{
+    /// <summary>
+    /// Used for unit testing duplex streams
+    /// </summary>
+    public class DuplexCommunicationPipe : IDisposable
+    {
+        private DuplexHalf _StreamOne;
+        private DuplexHalf _StreamTwo;
+
+        public DuplexCommunicationPipe()
+        {
+            _StreamOne = new DuplexHalf("One");
+            _StreamTwo = new DuplexHalf("Two");
+            _StreamOne.ReversePath = _StreamTwo;
+            _StreamTwo.ReversePath = _StreamOne;
+        }
+
+        public Stream Stream1 { get { return _StreamOne; } }
+        public Stream Stream2 { get { return _StreamTwo; } }
+
+        private class DuplexHalf : MemoryStream
+        {
+            private object _LockToken = new object();
+            private AutoResetEvent _ARE = new AutoResetEvent(false);
+            private WaitHandle[] _WaitHandles;
+            private string _Name;
+
+            public DuplexHalf(string name)
+            {
+                _Name = name;
+                _WaitHandles = new WaitHandle[] { _ARE };
+            }
+
+            public DuplexHalf ReversePath { get; set; }
+
+            public override void Write(byte[] buffer, int offset, int count)
+            {
+                ReversePath.BaseWrite(buffer, offset, count);
+            }
+ 
+            public override void WriteByte(byte value)
+            {
+                ReversePath.BaseWriteByte(value);
+            }
+
+            private int _LastRead = 0;
+            private void BaseWrite(byte[] buffer, int offset, int count)
+            {
+                Trace.WriteLine(string.Format("{0} BaseWrite() Acquiring Lock: Position: {1}, Last Read: {2} Length: {3}", _Name, Position, _LastRead, Length));
+                lock (_LockToken)
+                {
+                    Position = Length;
+                    base.Write(buffer, offset, count);
+                    Position = _LastRead;
+                }
+
+                Trace.WriteLine(string.Format("{0} BaseWrite() Setting ARE: Position: {1}, Last Read: {2} Length: {3}", _Name, Position, _LastRead, Length));
+                _ARE.Set();
+            } 
+
+            private void WaitForReadIfNeeded()
+            { 
+                if (Position == Length)
+                {
+                    Trace.WriteLine(string.Format("{0} WaitForReadIfNeeded() Waiting To Read: Position: {1}, Last Read: {2} Length: {3}", _Name, Position, _LastRead, Length));
+                    WaitHandle.WaitAll(_WaitHandles);
+                    Trace.WriteLine(string.Format("{0} WaitForReadIfNeeded() Waiting Finished: Position: {1}, Last Read: {2} Length: {3}", _Name, Position, _LastRead, Length));
+                } 
+            }
+
+            public override int Read(byte[] buffer, int offset, int count)
+            {
+                WaitForReadIfNeeded();
+                Trace.WriteLine(string.Format("{0} Read Acquiring Lock: Position: {1}, Last Read: {2} Length: {3}", _Name, Position, _LastRead, Length));
+                lock (_LockToken)
+                {
+                    int read = base.Read(buffer, offset, count);
+                    _LastRead += read;
+
+                    Trace.WriteLine(string.Format("{0} Read Finished: Position: {1}, Last Read: {2} Length: {3}", _Name, Position, _LastRead, Length));
+                    return read;
+                }
+            }
+
+
+            private void BaseWriteByte(byte value)
+            {
+                lock (_LockToken)
+                {
+                    Position = Length;
+                    base.WriteByte(value);
+                    Position = _LastRead;
+                }
+                _ARE.Set();
+            }
+
+            public override int ReadByte()
+            {                
+                WaitForReadIfNeeded();
+                lock (_LockToken)
+                { 
+                    int result = base.ReadByte();
+                    _LastRead += 1;
+                    return result;
+                }
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                _ARE.Set();            
+            }
+
+        }
+
+        public void Dispose()
+        {
+            Stream1.Dispose();
+            Stream2.Dispose();
+        }
+    }
+}
